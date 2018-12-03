a7513b06beb81ff5b07af0f62d84a8db9e17b016
Mwai Karimi <mwaik@outlook.com>
2018-12-03 15:47:54 +0100

    change desing, add url conf, add movie detail view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Movie
 
-# Create your views here.
+
+class MovieList(ListView):
+    model = Movie
+
+
+class MovieDetail(DetailView):
+    model = Movie
+
+
+
+
+
